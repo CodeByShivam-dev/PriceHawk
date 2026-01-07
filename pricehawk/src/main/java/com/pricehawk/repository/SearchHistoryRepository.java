188d747d7661ba10e8ed076f7341294b168359bb
CodeByShivam-dev <shivamkumar.dev2006@gmail.com>
2026-01-07 09:00:20 +0530

    docs(repository): improve documentation comments in SearchHistoryRepository

--- pricehawk/src/main/java/com/pricehawk/repository/SearchHistoryRepository.java
+++ pricehawk/src/main/java/com/pricehawk/repository/SearchHistoryRepository.java
@@ -5,13 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository for SearchHistory. We keep it simple for now; advanced queries
- * (top searches, timeframe queries) can be added as custom repository methods or
- * using @Query when needed.
+ * Repository for SearchHistory.
  */
 @Repository
-public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long>
-{
-    // Example extension methods you can add later:
-    // List<SearchHistory> findTop100ByQueryNormalizedOrderBySearchedAtDesc(String q);
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
 }
+
+
+
